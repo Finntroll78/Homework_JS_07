dd8099af0c4e06f2d80b96aca82cff808eaebf27
Сергей <finn.troll@mail.ru>
2019-04-23 16:32:33 +0300

    дз

--- lesson-3/option_task_1/js.js
+++ lesson-3/option_task_1/js.js
@@ -1,15 +1,18 @@
 let str = 'урок-3-был слишком легким';
 
-let newStr = str[0].toUpperCase() + str.slice(1);
-console.log(newStr.replace(/-/g, ' '));
+let newStr = str[0].toUpperCase() + str.slice(1).replace(/-/g, ' ');
+console.log(newStr);
+let newStr1 = newStr.split(' ').splice(4,1).join().split('');
+newStr1.pop();
+newStr1.pop();
+newStr1.push('о','о');
+let newStr2 = newStr1.join('');
+console.log(newStr2)
 
-newStr1 = (newStr.split(' ').splice(2,1).join(newStr).split('').splice(4,2,'о','о').join(''));
-
-console.log(newStr1);
 
 /////////////////////////////////////////
 
-let arr = [20, 33, 1, 'Человек', 2, 3];
+/*let arr = [20, 33, 1, 'Человек', 2, 3];
 arr.splice(3,1);
 
 let newArr = [],
@@ -35,4 +38,4 @@ function fu(a) {
 	}
 }
 
-fu(' vffvf ')
+fu(' vffvf ')*/
