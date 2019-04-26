ac44238a5c1cec4e4212c2748d08ab6716621232
Сергей <finn.troll@mail.ru>
2019-04-26 11:57:09 +0300

    дз

--- lesson-5/main_task/js.js
+++ lesson-5/main_task/js.js
@@ -1,4 +1,12 @@
+nextPrime:
+  for (var i = 2; i <= 100; i++) {
 
+    for (var j = 2; j < i; j++) {
+      if (i % j == 0) continue nextPrime;
+    }
+
+    console.log(`Делители числа ${i}: 1 и ${i}`); 
+  }
 
 
 
